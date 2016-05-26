090cbb0de761ee9842e3bb5c06e1fcdec357509d
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-05-26 10:28:43 +0200

    Some building.

--- src/Network/Nats/Builder.hs
+++ src/Network/Nats/Builder.hs
@@ -7,14 +7,37 @@ module Network.Nats.Builder
 import Data.ByteString (ByteString)
 import Data.ByteString.Builder
 import Data.Monoid ((<>))
+import Data.List (foldl', intersperse)
 
 import Network.Nats.Message (Message (..))
 
--- | Translate a Message record to a Builder.
-buildMessage :: Message -> Builder
-buildMessage msg@Info {..} =
-    byteString "INFO {" <>
-    charUtf8 '}'
+class Buildable a where
+    build :: a -> Builder
 
-handshakeMessageField :: ByteString -> a -> Builder
-handshakeMessageField = undefined
+instance Buildable Bool where
+    build False = byteString "false"
+    build True  = byteString "true"
+
+instance Buildable Int where
+    build = intDec
+
+instance Buildable ByteString where
+    build value = charUtf8 '\"' <> byteString value <> charUtf8 '\"'
+
+-- | Translate a Message record to a Builder. The building of handshake
+-- messages are not optimized for speed :-)
+buildMessage :: Message -> Builder
+buildMessage Info {..} =
+    let fields = foldl' handshakeMessageField [] 
+                        [ ("\"server_id\"", serverId)
+                        ]
+        fields' = intersperse (charUtf8 ',') $ reverse fields
+    in mconcat $ byteString "INFO {":(fields' ++ [charUtf8 '}'])
+
+handshakeMessageField :: Buildable a => [Builder] 
+                      -> (ByteString, Maybe a) 
+                      -> [Builder]
+handshakeMessageField xs (field, (Just value)) =
+    let x = byteString field <> charUtf8 ':' <> build value
+    in x:xs
+handshakeMessageField xs (_, Nothing) = xs
