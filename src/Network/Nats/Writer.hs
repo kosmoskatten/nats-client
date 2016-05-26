7c809c4b8e8f2fc957eee9d7ccd43880d8837e60
Patrik Sandahl <patrik.sandahl@ericsson.com>
2016-05-26 12:43:40 +0200

    Renaming of Builder => Writer.

--- src/Network/Nats/Writer.hs
+++ src/Network/Nats/Writer.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE OverloadedStrings         #-}
 {-# LANGUAGE RecordWildCards           #-}
-module Network.Nats.Builder
-    ( buildMessage
+module Network.Nats.Writer
+    ( writeMessage
     ) where
 
 import Data.ByteString (ByteString)
@@ -13,31 +13,31 @@ import Data.List (foldl', intersperse)
 import Network.Nats.Message (Message (..))
 
 -- | Existentially quantified Field type, to allow for a polymorph
--- list of Fields. All fields with the contraint of beeing Buildable.
-data Field = forall b. Buildable b => Field !b
+-- list of Fields. All fields with the contraint of beeing Writeable.
+data Field = forall w. Writeable w => Field !w
 
--- | Helper class used for the building of "handshake message" fields.
-class Buildable a where
-    build :: a -> Builder
+-- | Helper class used for the writing of "handshake message" fields.
+class Writeable w where
+    write :: w -> Builder
 
 -- | Instance for Bool.
-instance Buildable Bool where
-    build False = byteString "false"
-    build True  = byteString "true"
+instance Writeable Bool where
+    write False = byteString "false"
+    write True  = byteString "true"
 
 -- | Instance for Int.
-instance Buildable Int where
-    build = intDec
+instance Writeable Int where
+    write = intDec
 
 -- | Instance for ByteString.
-instance Buildable ByteString where
-    build value = charUtf8 '\"' <> byteString value <> charUtf8 '\"'
+instance Writeable ByteString where
+    write value = charUtf8 '\"' <> byteString value <> charUtf8 '\"'
 
 -- | Translate a Message record to a Builder. The building of handshake
 -- messages are not optimized for speed :-)
-buildMessage :: Message -> Builder
-buildMessage Info {..} =
-    let fields = foldl' buildField [] 
+writeMessage :: Message -> Builder
+writeMessage Info {..} =
+    let fields = foldl' writeField [] 
                    [ ("\"server_id\"", Field <$> serverId)
                    , ("\"version\"", Field <$> serverVersion)
                    , ("\"go\"", Field <$> goVersion)
@@ -54,10 +54,10 @@ buildMessage Info {..} =
 
 -- | The translate a Field to a Builder and prepend it to the list of
 -- Builders.
-buildField :: [Builder] -> (ByteString, Maybe Field) -> [Builder]
-buildField xs (name, Just (Field value)) = 
-    let x = byteString name <> charUtf8 ':' <> build value
+writeField :: [Builder] -> (ByteString, Maybe Field) -> [Builder]
+writeField xs (name, Just (Field value)) = 
+    let x = byteString name <> charUtf8 ':' <> write value
     in x:xs
 
 -- There's a Nothing Field. Just return the unmodified Builder list.
-buildField xs (_, Nothing) = xs
+writeField xs (_, Nothing) = xs
