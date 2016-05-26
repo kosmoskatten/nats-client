832bf2bf96dfcdd95bdc1495f12dd38bd553eb74
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-05-26 18:27:53 +0200

    Property test for encode decode Message works.

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Test.Framework (Test, defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+import Network.Nats.MessageProps (encodeDecodeMessage)
+
 main :: IO ()
-main = return ()
+main = defaultMain testSuite
+
+testSuite ::  [Test]
+testSuite =
+    [ testGroup "Message property tests"
+        [ testProperty "Encoding and decoding of Message"
+                       encodeDecodeMessage
+        ]
+    ]
--- test/Network/Nats/MessageProps.hs
+++ test/Network/Nats/MessageProps.hs
@@ -16,12 +16,13 @@ import Network.Nats.Message (Message (..))
 import Network.Nats.Parser (message)
 import Network.Nats.Writer (writeMessage)
 
+-- | Arbitrary instance for Message.
 instance Arbitrary Message where
     arbitrary =
-        Info <$> perhaps quotedString
-             <*> perhaps quotedString
-             <*> perhaps quotedString
-             <*> perhaps quotedString
+        Info <$> perhaps valueString
+             <*> perhaps valueString
+             <*> perhaps valueString
+             <*> perhaps valueString
              <*> perhaps posInt
              <*> arbitrary
              <*> arbitrary
@@ -29,6 +30,7 @@ instance Arbitrary Message where
              <*> arbitrary
              <*> perhaps posInt
 
+-- | Test by write a Message to ByteString, and parse it back again.
 encodeDecodeMessage :: Message -> Bool
 encodeDecodeMessage msg =
     let asByteString = LBS.toStrict $ toLazyByteString $ writeMessage msg
@@ -41,13 +43,14 @@ encodeDecodeMessage msg =
             (Partial g)   -> verify (g "")
             _             -> False
 
+-- | Generate Maybe's for the tailor made generators.
 perhaps :: Gen a -> Gen (Maybe a)
 perhaps gen = oneof [ return Nothing, Just <$> gen ]
 
-quotedString :: Gen ByteString
-quotedString = do
-    xs <- BS.pack <$> listOf (elements selection)
-    return $ '\"' `BS.cons` (xs `BS.snoc` '\"')
+-- | Generate a ByteString which not contains any quote characters.
+valueString :: Gen ByteString
+valueString =
+    BS.pack <$> listOf (elements selection)
     where
       selection :: [Char]
       selection = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "+-_!?*(){} "
